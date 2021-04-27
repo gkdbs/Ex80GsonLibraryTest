ff5f026fa98aab444815e9e563a6299f64bdc713
하윤 <gkdbs514@naver.com>
2021-04-27 16:48:12 +0900

    Initial commit

---
+++ app/src/main/java/com/mrhi2020/ex80gsonlibrarytest/Person.java
@@ -0,0 +1,14 @@
+package com.mrhi2020.ex80gsonlibrarytest;
+
+public class Person {
+    String name;
+    int age;
+
+    public Person() {
+    }
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
